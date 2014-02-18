eda8a61bd424b6e0e918dd5e7d0e225c86290812
wzhscript <whscript@gmail.com>
2014-02-18 22:32:25 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'TiebaSniffer' });
+};
+
+exports.start = function(req, res){
+
+};
+
+exports.stop = function(req, res){
+	
+};
