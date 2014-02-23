5d9f108780b1556f554ba64ceace75fd677da29d
wzhscript <whscript@gmail.com>
2014-02-23 23:06:25 +0800

    enable database, no test

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ var cheerio = require("cheerio");
 var iconv = require('iconv-lite');
 var BufferHelper = require('bufferhelper');//用于拼接BUffer。防止中文单词断裂
 var process = require('child_process');
+var fs = require('fs');
+var Tiezi = require('./models/tiezi.js');
 
 
 // Utility function that downloads a URL and invokes
@@ -28,8 +30,56 @@ function download(url, callback) {
 
 function sendMail(title, content){
 	content = content.replace(/"/g,"\\\"");
-	var str = 'echo "' + content + '"|mail -s "$(echo -e "' + title + '\nContent-Type: text/html")" wzhscript@126.com';
+
+	var str = 'echo "' + content + '"|mail -s "$(echo "' + title + '\nContent-Type: text/html; charset=UTF-8")" wzhscript@126.com';
 	process.exec(str);
+}
+
+
+function sendMailF(title, content){
+	var filename = "mail.txt";
+
+	fs.open( filename, 'w', 0666, function(e, fd) {
+	    if(e) {
+	        console.log('错误信息：' + e);
+	    } else {
+	        fs.write(fd, content, 0, 'utf8', function(e) {
+	            if(e) {
+	                console.log('出错信息：' + e);
+	            } else {
+	                fs.closeSync(fd);
+
+	                var str = 'mail -s "$(echo "' + title + '\nContent-Type: text/html; charset=UTF-8")" wzhscript@126.com <'+filename;
+	                process.exec(str);
+	            }
+	        });
+	    }
+	});
+}
+
+function sendTiezi(tiezi){
+	sendMail(tiezi.title,tieze.content);
+}
+
+function tackleTiezi(title, content){
+	var newTiezi = new Tiezi({
+		title: title,
+		content: content
+	});
+
+	Tiezi.get(newTiezi.title, function(err, tiezi){
+		if(tiezi){
+			return;
+		}
+		//如果不存在，则新增帖子
+		newTiezi.save(function(err, tiezi){
+			if(err){
+				console.log("err: fail to save tiezi "+ newTiezi.title);
+			}
+
+			sendTiezi(tiezi);
+		});
+	});
 }
 
 exports.index = function(req, res){
@@ -38,52 +88,48 @@ exports.index = function(req, res){
 
 exports.start = function(req, res){
 	var url = "http://tieba.baidu.com/f?kw=%C3%A7%BB%C4%BC%CD";
-	download(url, function(data) {
-	  if (data) {
-	    var $ = cheerio.load(data);
-	    console.log("ok"+$("ul#thread_list img[src*='zding.gif']").length);
-	    $("ul#thread_list img[src*='zding.gif']").each(function(i, img) {
-	    	var $link = $(img).parent().parent().find("a[href^='/p/']").first();
-	    	var title = $link.text();
-	        if(i===0){
-            	console.log(title);
-	        }
+	res.send("started");
+	var count = 0;
+//	setInterval(function(){
+		download(url, function(data) {
+		  if (data) {
+		    var $ = cheerio.load(data);
+		    console.log("count = "+(count++));
+		    $("ul#thread_list img[src*='zding.gif']").each(function(i, img) {
+		    	var $link = $(img).parent().parent().find("a[href^='/p/']").first();
+		    	var title = $link.text();//get the title of tiezi
 
-	        var temp_url = "http://tieba.baidu.com"+$link.attr("href");
-	        download(temp_url,function(data){
-	        	var $ = cheerio.load(data);
+	            console.log(title);
+		        
+		        //get the url of tiezi
+		        var temp_url = "http://tieba.baidu.com"+$link.attr("href");
 
-	        	//$("a").remove();
-	        	$("a").each(function(i, e){
-	        		var text = $(e).text();
-	        		$(e).replaceWith(text);
-	        	});
+		        //get the content of tiezi
+		        download(temp_url,function(data){
+		        	var $ = cheerio.load(data);
 
-	        	var $contentDiv = $("div#j_p_postlist > div").first().find("div[id^=post_content]").first();
-	        	//$contentDiv.find("a").remove();
-	        	//$contentDiv.remove("a");
-	        	var content = $('div','<div></div>').append($contentDiv).html();
+		        	//remove hyperlinks
+		        	$("a").each(function(i, e){
+		        		var text = $(e).text();
+		        		$(e).replaceWith(text);
+		        	});
 
-		        if(i===1){
-	            	console.log(content);
-		        	res.send(title + "<br />" + content); 
-		        }
-	        	sendMail(title,content);
-	        });
-			// Document temp_doc = Jsoup.connect(temp_url).get();
-			// Element contentDiv = temp_doc.select("div#j_p_postlist>div").first().select("div[id^=post_content]").first();
-			// contentDiv.select("a").remove();
-			// String content = contentDiv.outerHtml();
-			
-			
-	      });
+		        	var $contentDiv = $("div#j_p_postlist > div").first().find("div[id^=post_content]").first();
+		        	var content = $('div','<div></div>').append($contentDiv).html();
 
-	    console.log("done");
-	  }
-	  else console.log("error");  
-	});
+		        	tackleTiezi(title,content);
+		        });
+		      });
+
+		    console.log("done");
+		  }
+		  else console.log("error");  
+		});		
+//	},1000*60);
+	
 };
 
 exports.stop = function(req, res){
 
 };
+
